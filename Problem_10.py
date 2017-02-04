57bea317413775fc5208cea541b4e6923af9bca0
조인영 (In-Young Cho) <ciy405x@kaist.ac.kr>
2017-02-04 21:31:14 +0900

    Update Problem_10.py

--- Problem_10.py
+++ Problem_10.py
@@ -6,7 +6,7 @@ The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 Find the sum of all the primes below two million.
 """
 
-#Brute Force Prime Finder
+#Brute Force Prime Finder n<200000
 def BF(n):
     prime_arr = [2]
     num = 3
@@ -23,3 +23,21 @@ def BF(n):
         num += 1
     print prime_arr
     return reduce(lambda x,y : x + y, prime_arr)
+
+#Brute Force second ver.
+import math
+
+def BF2(n):
+    result = 0;
+    i = 2
+    while i < n:
+        if isPrime(i) == 1:
+            result += i
+        i += 1
+    return result
+
+def isPrime(i):
+    for j in range(2,int(math.sqrt(i))+1):
+        if i % j == 0:
+            return 0
+    return 1
