fe050d037d8e4b7240da24641c6d7fb312c3394b
조인영 (In-Young Cho) <ciy405x@kaist.ac.kr>
2017-02-06 15:10:45 +0900

    Create Problem_17.py

---
+++ Problem_17.py
@@ -0,0 +1,43 @@
+# Problem_17: Number letter counts
+"""
+Q.
+If the numbers 1 to 5 are written out in words: one, two, three, four, five, then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
+
+If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, how many letters would be used?
+"""
+#Parse them and count them. The answer is as below 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#21124
